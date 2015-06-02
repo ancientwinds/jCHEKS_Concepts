70f1040606b3c5feed57fd9b7b58ea9392d8fac9
etudiant <etudiant@etudiant1>
2015-06-02 10:31:11 -0400

    Added the AbstractCommunicator class Temp: Added the Communication class. Should be an interface/abstract.

---
+++ src/com/archosResearch/jCheks/concept/communicator/AbstractCommunicator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.archosResearch.jCheks.concept.communicator;
+
+/**
+ *
+ * @author Thomas Lepage
+ */
+public abstract class AbstractCommunicator{
+    //TODO eventually move this class into the Conceps project
+    //TODO do not use the Java Observable mechanism, build our own instead so it will be tailored to our needs.     
+    public abstract void sendCommunication(Communication aCommunication);
+    
+}
