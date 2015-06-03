0c37455b0cf8419b17dba490455f0fe78c491b6f
Michael Roussel <rousselm4@gmail.com>
2015-06-03 14:54:21 -0400

    Add abstract method in AbstractEngine.

--- src/com/archosResearch/jCHEKS/concept/engine/AbstractEngine.java
+++ src/com/archosResearch/jCHEKS/concept/engine/AbstractEngine.java
@@ -2,6 +2,8 @@ package com.archosResearch.jCHEKS.concept.engine;
 
 /**
  *
- * @author Thomas Lepage thomas.lepage@hotmail.ca
+ * @author Thomas Lepage thomas.lepage@hotmail.ca & Michael Roussel <rousselm4@gmail.com>
  */    
-public abstract class AbstractEngine{}
+public abstract class AbstractEngine{
+    public abstract void handleOutgoingMessage(String content, String contactName);
+}
