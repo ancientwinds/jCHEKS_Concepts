a10eb113381203b9cec78527ab51adf761b87847
etudiant <etudiant@etudiant2>
2015-06-04 09:49:06 -0400

    Add InputOutputManager

---
+++ src/com/archosResearch/jCheks/concept/ioManager/InputOutputManager.java
@@ -0,0 +1,19 @@
+package com.archosResearch.jCheks.concept.ioManager;
+
+import com.archosResearch.jCHEKS.concept.engine.AbstractEngine;
+import com.archosResearch.jCHEKS.concept.engine.ModelObserver;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+
+//Todo In concept project?
+public interface InputOutputManager extends ModelObserver {
+
+    public void setEngine(AbstractEngine engine);
+
+    public void forwardOutgoingMessage(String messageContent);
+
+    public void setSelectedContactName(String name);
+}
