d0f5700709cb0c2c8fb72a722162a5806bb2c70a
LepageT <thomas.lepage@hotmail.ca>
2015-06-15 13:37:24 -0400

    Communicator now only have one observer.

--- src/com/archosResearch/jCHEKS/concept/communicator/CommunicatorObservable.java
+++ src/com/archosResearch/jCHEKS/concept/communicator/CommunicatorObservable.java
@@ -5,25 +5,23 @@ package com.archosResearch.jCHEKS.concept.communicator;
  * @author Thomas Lepage thomas.lepage@hotmail.ca
  */
 
-public class CommunicatorObservable extends AbstractObservable<CommunicatorObserver>{
+public class CommunicatorObservable {
+    
+    private CommunicatorObserver observer;
+    
+    public void addObserver(CommunicatorObserver observer) {
+        this.observer = observer;
+    }
     
     public String notifyCommunicationReceived(AbstractCommunication communication){
-        /*for(CommunicatorObserver observer: this.observers){
-            observer.communicationReceived(communication);
-        }*/
-        //TODO Only one observer.
-        return this.observers.iterator().next().communicationReceived(communication);
+        return this.observer.communicationReceived(communication);
     }
     
     public void notifyAckReceived(){
-        for(CommunicatorObserver observer: this.observers){
-            observer.ackReceived();
-        }
+        this.observer.ackReceived();
     }
     
     public void notifySecureAckReceived() {
-        for(CommunicatorObserver observer: this.observers){
-            observer.secureAckReceived();
-        }
+        this.observer.secureAckReceived();
     }
 }
