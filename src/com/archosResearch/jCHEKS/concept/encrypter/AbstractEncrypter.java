52ddb4acf565ebc62e20d0343cb6c8601b13eab4
LepageT <thomas.lepage@hotmail.ca>
2015-06-16 11:59:39 -0400

    Encrypter methods need to return bytes array.

--- src/com/archosResearch/jCHEKS/concept/encrypter/AbstractEncrypter.java
+++ src/com/archosResearch/jCHEKS/concept/encrypter/AbstractEncrypter.java
@@ -1,10 +1,13 @@
 package com.archosResearch.jCHEKS.concept.encrypter;
 
+import com.archosResearch.jCHEKS.concept.chaoticSystem.AbstractChaoticSystem;
+
 /**
  *
  * @author fgagnon
  */
 public abstract class AbstractEncrypter {
 
-    public abstract void encrypt();
+    public abstract byte[] encrypt(byte[] text, AbstractChaoticSystem chaoticSystem);
+    public abstract byte[] decrypt(byte[] text, AbstractChaoticSystem chaoticSystem);
 }
