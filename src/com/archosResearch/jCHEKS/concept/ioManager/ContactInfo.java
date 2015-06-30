f64d3c1131220e589ce68f67808201ea97fa9f8a
etudiant <etudiant@etudiant2>
2015-06-30 14:34:37 -0400

    Remove unique id

--- src/com/archosResearch/jCHEKS/concept/ioManager/ContactInfo.java
+++ src/com/archosResearch/jCHEKS/concept/ioManager/ContactInfo.java
@@ -9,15 +9,15 @@ public class ContactInfo {
     private final String ip;
     private final int port;
     private final String name;
-    private String uniqueId;
     private String receivingChaoticSystem;
     private String sendingChaoticSystem;
 
-    public ContactInfo(String ip, int port, String name, String uniqueId) {
+    public ContactInfo(String ip, int port, String name, String receivingChaoticSystemPath, String sendingChaoticSystemPath) {
         this.ip = ip;
         this.port = port;
         this.name = name;
-        this.uniqueId = uniqueId;
+        this.receivingChaoticSystem = receivingChaoticSystemPath;
+        this.sendingChaoticSystem = sendingChaoticSystemPath;
     }
 
     public String getIp() {
@@ -30,25 +30,6 @@ public class ContactInfo {
 
     public String getName() {
         return name;
-    }
-
-    public String getUniqueId() {
-        return uniqueId;
-    }
-
-    public void generateUniqueId(String currentIp) {
-        if (currentIp.hashCode() > this.ip.hashCode()) {
-            this.uniqueId = this.ip + currentIp;
-        } else {
-            this.uniqueId = currentIp + this.ip;
-        }
-    }
-    
-    public void generateChaoticSystemName(String contactName) {
-        String ownName = this.name.replace("-", "");
-        String otherName = contactName.replace("-", "");
-        this.receivingChaoticSystem = otherName + "-" + ownName + ".xml";
-        this.sendingChaoticSystem = ownName + "-" + otherName + ".xml";
     }
     
     public String getReceivingChaoticSystem() {
