26b38c11e2f76e20f25a88b2293e977d24fc533c
LepageT <thomas.lepage@hotmail.ca>
2015-07-07 12:51:29 -0400

    Refactor avec Michael.

--- src/com/archosResearch/jCHEKS/concept/chaoticSystem/AbstractChaoticSystem.java
+++ src/com/archosResearch/jCHEKS/concept/chaoticSystem/AbstractChaoticSystem.java
@@ -6,14 +6,11 @@ package com.archosResearch.jCHEKS.concept.chaoticSystem;
  */
 public abstract class AbstractChaoticSystem {
 
-    //<editor-fold defaultstate="collapsed" desc="Properties">
     protected String systemId;
     protected int keyLength;
     protected final int maxImpact = 32;
     protected byte[] lastGeneratedKey;
-    //</editor-fold>
 
-    //<editor-fold defaultstate="collapsed" desc="Abstract methods">
     public abstract void evolveSystem(int factor);
 
     public abstract byte[] getKey(int requiredLength) throws Exception;
@@ -28,9 +25,7 @@ public abstract class AbstractChaoticSystem {
     public abstract void deserialize(String serialization);
 
     protected abstract void generateSystem(int keyLength) throws Exception;
-    //</editor-fold>
     
-    //<editor-fold defaultstate="collapsed" desc="Constructors">
     
     public AbstractChaoticSystem(int keyLength) throws Exception {
         this(keyLength, java.util.UUID.randomUUID().toString());
@@ -43,20 +38,14 @@ public abstract class AbstractChaoticSystem {
     
     protected AbstractChaoticSystem() { }
 
-    //</editor-fold>
-
-    //<editor-fold defaultstate="collapsed" desc="Methods">
     public byte[] getKey() {
         return this.lastGeneratedKey;
     }
 
     public void evolveSystem() {
         this.evolveSystem(0);
-    }
+    }    
     
-    //</editor-fold>
-    
-    //<editor-fold defaultstate="collapsed" desc="Accessors">
     public String getSystemId() {
         return this.systemId;
     }
@@ -64,6 +53,5 @@ public abstract class AbstractChaoticSystem {
     public int getImpact() {
         return this.maxImpact;
     }
-    //</editor-fold>
 
 }
